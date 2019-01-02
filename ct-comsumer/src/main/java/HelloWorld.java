db33e836e7ee8076bdfb498b8333a7d1719b5800
wzt <2216104644@qq.com>
2019-01-02 15:45:58 +0800

    提交剑法

---
+++ ct-comsumer/src/main/java/HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        //Test
+        System.out.println("helloworld");
+        System.out.println("------------");
+    }
+}
